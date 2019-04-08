f859e54c681409310aa44918671c0e96370e2623
BackendBibek <41180200+BackendBibek@users.noreply.github.com>
2019-04-08 06:56:29 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
